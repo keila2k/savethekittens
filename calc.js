c5e71c4bce05319fe43e845c96f068b58483ee0c
keila <ori.keila@gmail.com>
2018-07-26 11:17:18 +0300

    first commit

---
+++ calc.js
@@ -0,0 +1,23 @@
+
+
+var form = document.getElementById('xIsWhatPercentOfY');
+form.addEventListener('submit',function(event) {
+    
+    var resultField = document.getElementById('resultField');
+    var numField1 = document.getElementById('numField1');
+    var numField2 = document.getElementById('numField2');
+    
+    if ( !numField1.value || !numField2.value ){
+        alert("Please enter values in the fields");
+    }
+    else {
+        var x = parseFloat(numField1.value);
+        var y = parseFloat(numField2.value);
+        var res = ((x/y) * 100);
+        resultField.innerText = "Answer: " + res + "%";
+        event.preventDefault();
+        
+    }
+});
+    
+    
